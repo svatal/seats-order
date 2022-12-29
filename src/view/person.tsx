148f5b5529fe3a2383437a41d53f84d9125d98ef
svatal <swata@centrum.cz>
2022-12-29 14:27:03 +0100

    display happiness also with number

---
+++ src/view/person.tsx
@@ -0,0 +1,32 @@
+import * as b from "bobril";
+import { IPerson } from "../data/data";
+
+export function Person(p: { person: IPerson; happiness: number }) {
+  const { person, happiness } = p;
+  return (
+    <div
+      style={{
+        width: 98,
+        height: person.plusOne ? 38 : 18,
+        // backgroundColor: "gray",
+        backgroundColor: getColor(happiness),
+        borderStyle: "solid",
+        borderColor: "black",
+        borderWidth: 1,
+        boxSizing: "border-box",
+        margin: 1,
+        marginBottom: 2,
+      }}
+    >
+      {person.name} - {happiness.toFixed(2)}
+    </div>
+  );
+}
+
+function getColor(happiness: number) {
+  return `#${colorFragment(1 - happiness)}${colorFragment(happiness)}00`;
+}
+
+function colorFragment(n: number) {
+  return Math.round(Math.min(255 * n * 2, 255)).toString(16);
+}
--- src/view/table.tsx
+++ src/view/table.tsx
@@ -2,6 +2,7 @@ import * as b from "bobril";
 import { getHappiness } from "../alg/eval";
 import { ITable } from "../data/data";
 import { peopleList, seating } from "../data/model";
+import { Person } from "./person";
 
 export function Table({ table }: { table: ITable }) {
   const persons = peopleList.filter((p) => seating[p.id] === table.id);
@@ -9,37 +10,14 @@ export function Table({ table }: { table: ITable }) {
     <div
       style={{
         width: 100,
-        height: table.seatCount * 20,
+        height: table.seatCount * 20 + 20,
         backgroundColor: "lightGray",
       }}
     >
+      <div style={{ height: 20 }}>Table #{table.id}</div>
       {persons.map((p) => (
-        <div
-          style={{
-            width: 98,
-            height: p.plusOne ? 38 : 18,
-            // backgroundColor: "gray",
-            backgroundColor: getColor(p.id),
-            borderStyle: "solid",
-            borderColor: "black",
-            borderWidth: 1,
-            boxSizing: "border-box",
-            margin: 1,
-            marginBottom: 2,
-          }}
-        >
-          {p.name}
-        </div>
+        <Person person={p} happiness={getHappiness(p.id)} />
       ))}
     </div>
   );
-}
-
-function getColor(personId: number) {
-  const happiness = getHappiness(personId);
-  return `#${colorFragment(1 - happiness)}${colorFragment(happiness)}00`;
-}
-
-function colorFragment(n: number) {
-  return Math.round(Math.min(255 * n * 2, 255)).toString(16);
 }
